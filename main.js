cb10c3b45aeb8cd50f1392e6f013e3163e9a61cb
theTrainMan <stormbreaker70@gmail.com>
2021-01-18 10:38:28 +0200

    First Create
    Created spreadsheets.
    Comments
    What is Variable
    Assigning a Value

---
+++ main.js
@@ -0,0 +1,9 @@
+// This is an in-line comment.
+/* This is a 
+multi-line comment
+*/
+
+// Declare JavaScript Variables
+/*
+In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+*/
