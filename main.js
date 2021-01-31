263c3afc466d92fd6cad7ecacd3e6ccdaf45a8cb
theTrainMan <stormbreaker70@gmail.com>
2021-01-31 10:11:15 +0200

    Recursion Countdown

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ multi-line comment
 /*
 In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
 */
+let countArray = 0;
+
+function countUp(n) {
+    if (n < 3) {
+        return [];
+    } else {
+        const countArray = countUp(n - 1);
+        countArray.push(n);
+        return countArray;
+    }
+}
+console.log(countUp(10));
